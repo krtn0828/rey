c43a584ab7c0dfc18696268c51ef205318a8550c
karaton <75928242+krtn0828@users.noreply.github.com>
2021-11-19 23:41:11 +0900

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,1 +1,8 @@
-
+export const fbaseConfig = {
+  apiKey: 'YOUR_FIREBASE_PROJECT_API_KEY',
+  authDomain: 'YOUR_FIREBASE_PROJECT.firebaseapp.com',
+  databaseURL: 'YOUR_FIREBASE_PROJECT.firebaseio.com',
+  projectId: 'YOUR_FIREBASE_PROJECT',
+  storageBucket: 'YOUR_FIREBASE_PROJECT.appspot.com',
+  messagingSenderId: 'YOUR_FIREBASE_PROJECT_MSG_SEN_ID',
+};
