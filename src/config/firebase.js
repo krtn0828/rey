f20df91de82961d5224949a7410f063c759f3021
karaton <75928242+krtn0828@users.noreply.github.com>
2021-11-20 20:23:57 +0900

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,8 @@
 export const fbaseConfig = {
   apiKey: 'AIzaSyA2Frv1HO4DlwJDm2ZNqX5daTr_oTyXXRk',
   authDomain: 'rey-yt.firebaseapp.com',
-  databaseURL: 'YOUR_FIREBASE_PROJECT.firebaseio.com',
+  databaseURL: 'rey-yt-default-rtdb.firebaseio.com',
   projectId: 'rey-yt',
-  storageBucket: 'YOUR_FIREBASE_PROJECT.appspot.com',
+  storageBucket: 'rey-yt.appspot.com',
   messagingSenderId: '580058051801',
 };
